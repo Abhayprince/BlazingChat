1fbdb40275efa7504056350e6bd31dc51aac24d4
Abhay Prince <46496449+Abhayprince@users.noreply.github.com>
2023-03-07 22:57:22 +0530

    Part - 10 Token Expiry Fix. Scrool to last message. Messages loader and UI changes

--- BlazingChat/Client/wwwroot/app.js
+++ BlazingChat/Client/wwwroot/app.js
@@ -6,3 +6,12 @@ window.getFromStorage = (key) => storage.getItem(key);
 window.setToStorage = (key, value) => storage.setItem(key, value);
 
 window.removeFromStorage = (key) => storage.removeItem(key);
+
+window.scrollToLastMessage = () => {
+    var lastMessageLi = document.querySelector('#messages-ul > li:last-child');
+    if (lastMessageLi) {
+        lastMessageLi.scrollIntoView({
+            behavior: 'smooth'
+        });
+    }
+}
